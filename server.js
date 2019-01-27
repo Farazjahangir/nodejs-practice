99639b43e8e1d510eafa83791a0414651a24fb09
farazjahangir <jahangirfaraz98@gmail.com>
2019-01-27 22:20:47 +0500

    SignIn/SignUp page developed

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ app.use(express.static(__dirname + "/views/assets"))
 app.get('/' , (req , res)=>{
     res.render('index')
 })
-
+app.get('/signup' , (req , res)=>{
+    res.render('signup')
+})
 app.listen(port , ()=>{
     console.log("Server is running on PORT" , port);
     
